634104894039b25105a294e236aa96cd7f505f06
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-09-06 19:53:52 +0100

    Added Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, Link } from "@mui/material";
+import { CTA } from "./CTA";
+import { HeaderSocials } from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Box className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Lukasz Adamczyk</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <Link href="#contact" className="scroll_down">
+          Scroll Down
+        </Link>
+      </Box>
+    </header>
+  );
 };
 
 export default Header;
